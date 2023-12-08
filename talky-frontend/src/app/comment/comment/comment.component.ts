fc52961629cc68d20ff2f19f7e84ef32793d1a0b
isaac-kiptoo-kilimo <isaackilimok2@gmail.com>
2023-12-08 16:48:04 +0300

    feat:added the likes to toggle when liked

--- talky-frontend/src/app/comment/comment/comment.component.ts
+++ talky-frontend/src/app/comment/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comment',
@@ -8,5 +9,32 @@ import { Component } from '@angular/core';
 export class CommentComponent {
 
   hidden: boolean=true
+  isLikeClicked = false;
+  isLikeClicked2 = false;
+  isLikeClicked3 = false;
+
+constructor(private router:Router){
+
+}
+
+
+
+toggleLikeColor() {
+  this.isLikeClicked = !this.isLikeClicked;
+  console.log(this.isLikeClicked);
+  
+}
+
+
+toggleLikeColor2() {
+  this.isLikeClicked2 = !this.isLikeClicked2;
+  console.log(this.isLikeClicked2);
+  
+}
+toggleLikeColor3() {
+  this.isLikeClicked3 = !this.isLikeClicked3;
+  console.log(this.isLikeClicked3);
+  
+}
 
 }
