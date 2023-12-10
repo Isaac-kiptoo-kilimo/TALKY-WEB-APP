35a330feddde0297045496317f160f2c053a56e1
isaac-kiptoo-kilimo <isaackilimok2@gmail.com>
2023-12-10 14:51:07 +0300

    feat:added responsiveness to the message page

--- talky-frontend/src/app/post/post/post.component.ts
+++ talky-frontend/src/app/post/post/post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -6,6 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
+
+  hidden: boolean= true
+   isLikeClicked = false;
+  isLikeClicked2 = false;
+  isLikeClicked3 = false;
+  
+
+constructor(private router:Router){
+
+}
+
+
+
+toggleLikeColor() {
+  this.isLikeClicked = !this.isLikeClicked;
+  console.log(this.isLikeClicked);
+  
+}
+
+
+toggleLikeColor2() {
+  this.isLikeClicked2 = !this.isLikeClicked2;
+  console.log(this.isLikeClicked2);
+  
+}
+toggleLikeColor3() {
+  this.isLikeClicked3 = !this.isLikeClicked3;
+  console.log(this.isLikeClicked3);
+  
+}
+
   postComment(){
     console.log("I am clickable");
     
